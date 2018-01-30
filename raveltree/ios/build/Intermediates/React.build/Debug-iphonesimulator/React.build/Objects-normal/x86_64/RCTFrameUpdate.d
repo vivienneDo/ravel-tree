05f584995f8ff7e3ccd310c58cee7b8d838422c2
Frank Fusco <fr@nkfus.co>
2018-01-30 18:07:24 -0500

    Perform initial iOS and Android builds.

---
+++ raveltree/ios/build/Intermediates/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTFrameUpdate.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/frank/ravel-tree/raveltree/node_modules/react-native/React/Base/RCTFrameUpdate.m \
+  /Users/frank/ravel-tree/raveltree/node_modules/react-native/React/Base/RCTFrameUpdate.h \
+  /Users/frank/ravel-tree/raveltree/node_modules/react-native/React/Base/RCTUtils.h
---
+++ raveltree/ios/build/Intermediates/React.build/Debug-iphonesimulator/jschelpers.build/StaticAnalyzer/React/jschelpers/normal/x86_64/systemJSCWrapper.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/frank/ravel-tree/raveltree/node_modules/react-native/ReactCommon/jschelpers/systemJSCWrapper.cpp \
+  /Users/frank/ravel-tree/raveltree/node_modules/react-native/React/../ReactCommon/jschelpers/JSCWrapper.h
