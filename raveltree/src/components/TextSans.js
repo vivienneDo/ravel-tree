7ccb21ccd815339be48d34ba4070a12ef54825ea
Frank Fusco <fr@nkfus.co>
2018-02-13 20:38:17 -0500

    Add bold prop to TextSans component

--- raveltree/src/components/TextSans.js
+++ raveltree/src/components/TextSans.js
@@ -21,6 +21,7 @@ export default class TextSans extends React.Component {
     const {
       size,
       color,
+      bold,
     } = this.props;
 
     const textStyles = [styles.serif];
@@ -28,6 +29,7 @@ export default class TextSans extends React.Component {
     textStyles.push ([
       this.props.size ? {fontSize: size} : {fontSize: 12},
       this.props.color ? {color: color} : {color: '#282828'},
+      this.props.bold ? {fontWeight: bold} : {fontWeight: 400},
     ]);
 
     return (
