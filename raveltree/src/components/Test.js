a0266d8411c801152c83f3cee4f456a5ea5b95e8
Frank Fusco <fr@nkfus.co>
2018-02-15 09:55:07 -0500

    Add MessageCard component

--- raveltree/src/components/Test.js
+++ raveltree/src/components/Test.js
@@ -34,6 +34,7 @@ import ConceptPopup from './ConceptPopup'
 import AddPopup from './AddPopup'
 import ForkPopup from './ForkPopup'
 import MessageStub from './MessageStub'
+import MessageCard from './MessageCard'
 
 export default class Test extends Component {
   onPressTest () {
@@ -43,13 +44,19 @@ export default class Test extends Component {
   render (){
     return (
       <View style={{flexDirection: 'column', flex: 1}}>
-        <MessageStub
+        <MessageCard
             message={
               'Hey, Rebecca! I just read your newest passage for Shakespeare on Ice, and I have to say, you really knocked it out of the park. I adore your dialogue, especially the bits involving Falstaff. Truly a character who deserved the new life you\’ve given him—and truly a deserving writer to do so!\n\nI wanted to let you know I\’ll be starting a new ravel soon that I think you\’d really enjoy messing around with. I\’ll send you an invite as soon as it\’s ready!\n\nClint'}
             user={'Clint Lane Clover'}
             active
         />
         {/*}
+        <MessageStub
+            message={
+              'Hey, Rebecca! I just read your newest passage for Shakespeare on Ice, and I have to say, you really knocked it out of the park. I adore your dialogue, especially the bits involving Falstaff. Truly a character who deserved the new life you\’ve given him—and truly a deserving writer to do so!\n\nI wanted to let you know I\’ll be starting a new ravel soon that I think you\’d really enjoy messing around with. I\’ll send you an invite as soon as it\’s ready!\n\nClint'}
+            user={'Clint Lane Clover'}
+            active
+        />
         <ForkPopup
           ravel={'Cassius in Rome'}
           passageID={{number: '1', version: 'B'}}
