bb5c179b1745218eef492fddb677a1f3faedb8a0
Frank Fusco <fr@nkfus.co>
2018-02-23 11:24:47 -0500

    Modify TextHeader component to accept color and size props

--- raveltree/src/components/TextHeader.js
+++ raveltree/src/components/TextHeader.js
@@ -1,3 +1,9 @@
+// Author: Alex Aguirre
+// Created: 01/18/18
+// Modified: 02/23/18 by Frank Fusco (fr@nkfus.co)
+//
+// Standard header text component for RavelTree.
+
 import React, {Component} from 'react';
 import {
   AppRegistry,
@@ -8,14 +14,27 @@ import {
   TextInput
 } from 'react-native';
 
-// 1-18-18
-// Text Header
 
 export default class TextHeader extends Component<{}> {
   render() {
+    const {
+      color,
+      size,
+      testID,
+    } = this.props;
+
+    var textStyles = [styles.textStyle];
+
+    if (this.props.color) {
+      textStyles.push ({color: color});
+    }
+    if (this.props.size) {
+      textStyles.push ({fontSize: size});
+    }
+
     return (
       <View>
-         <Text style = {styles.textStyle}>
+         <Text style = {textStyles}>
            {this.props.children.toUpperCase ()}
          </Text>
       </View>
