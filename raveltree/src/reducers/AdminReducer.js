d9eb90ef773daca0d1c74ddfbcb08bfe0beeac52
Vivienne Do <viviennedo94@gmail.com>
2018-03-05 23:56:54 -0500

    Added Admin reducer

---
+++ raveltree/src/reducers/AdminReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    terms_of_service: ''
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+
+        case 'INSERT_TERMS_OF_SERVICE':
+            return {
+                ...state,
+                terms_of_service: action.payload
+            };   
+        default:
+            return state;
+    }
+}
