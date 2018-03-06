f897d4b46d403d822d8cc2337d3f75d5cc3b9ef0
Frank Fusco <fr@nkfus.co>
2018-03-06 09:00:46 -0500

    Move Firebase authentication from App to Screen

--- raveltree/src/App.js
+++ raveltree/src/App.js
@@ -1,5 +1,7 @@
 // Change Log
-// 03/03/18 VD Do - Added backend imports, added combined reducers, moved firebase db credential to actions/index.js
+// 03/03/18 VD - Added backend imports, added combined reducers, moved firebase db credential to actions/index.js
+// 03/05/18 FF - Moved maps and navigation switch to AppContainer and Screen screens, respectively.
+// 03/06/18 FF - Moved Firebase authentication to Screen screen.
 
 import React, { Component } from 'react';
 import {
@@ -9,34 +11,11 @@ import {
   View
 } from 'react-native';
 
-// import StatusBar from './components/StatusBar'
-// import NavBar from './components/NavBar'
-// import Login from './screens/Login';
-// import Loader from './Loader';
-// import MainPage from './screens/MainPage';
-// import Test from './screens/Test';
-//
-// import Messages from './screens/Messages';
-// import MessageThread from './screens/MessageThread';
-// import Notifications from './screens/Notifications';
-// import TermsAndPrivacy from './screens/TermsAndPrivacy';
-// import YourRavels from './screens/YourRavels';
-// import Profile from './screens/Profile';
-// import StartARavel from './screens/StartARavel'
-// import AddTags from './screens/AddTags'
-// import InviteParticipants from './screens/InviteParticipants'
-// import Splash from './screens/Splash'
-// import LoginEmail from './screens/LoginEmail'
-// import Home from './screens/Home'
-// import Explore from './screens/Explore'
-// import Ravel from './screens/Ravel'
-
 // Backend imports
-import firebase from 'firebase';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import Thunk from 'redux-thunk';
-import { connect } from 'react-redux';
+//import { connect } from 'react-redux';
 import rootReducer from './reducers/index';
 
 import AppContainer from './AppContainer';
@@ -48,20 +27,6 @@ const store = createStore (
 );
 
 export default class App extends Component {
-
-  state = { loggedIn: null};
-
-  componentWillMount() {
-    firebase.auth().onAuthStateChanged((user) => {
-      console.log('Authentication state has changed');
-      if (user) {
-        this.setState({loggedIn: true})
-      } else {
-        this.setState({loggedIn: false})
-      }
-    });
-  }
-
   render () {
     return (
       <Provider store={store}>
