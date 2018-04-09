bcaa67bd401b2e64c66e4b5ebd97e52d96798758
Frank Fusco <fr@nkfus.co>
2018-04-09 14:13:38 -0400

    Fix user image bug on ForkPopup component

--- raveltree/src/components/ForkPopup.js
+++ raveltree/src/components/ForkPopup.js
@@ -121,7 +121,7 @@ class ForkPopup extends React.Component {
               </View>
               <View style={styles.row2}>
                 <InputText width={'auto'} placeholder={'Type a passage name (e.g., "The Reckoning").'} onChangeText={(text) => this.onChangeTitle (text)} />
-                <UserImage {...this.props} size={26}/>
+                <UserImage {...this.props} userID={firebase.auth ().currentUser.uid} size={26}/>
               </View>
             </View>
             <View style={styles.passage}>
