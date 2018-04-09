da37009e934fed6ba4342d86eb2245dab60a5fea
Vivienne Do <viviennedo94@gmail.com>
2018-04-09 14:17:17 -0400

    Added files backend uses to test, do not commit to dev

--- raveltree/src/utils/FBLoginComponent.js
+++ raveltree/src/utils/FBLoginComponent.js
@@ -82,7 +82,7 @@ class FBLoginComponent extends Component {
                                     firebase.database().ref(`/users/${firebase.auth().currentUser.uid}/userProfile`)
                                     updateUserProfile(user, {first_name:results['first_name'],last_name:results['last_name'],bio:'',photoURL:'', stat_ravel_led:0, stat_passage_written:0, stat_ravel_contributed:0, 
                                         upvotes:0, ravel_points:0, email:m_email });
-                                        console.log('NEW UPDATE')
+
                                         // Do any initial user setup here 
                                         firebase.database().ref(`/master_user_key/${user.uid}`).set({
                                             user_uid: true})
@@ -93,6 +93,21 @@ class FBLoginComponent extends Component {
                                                 firebase.database().ref(`users/${user.uid}/ravel_created`).set(false)
                                         
                                             })
+                                } else {
+                                    // Check if they have been banned 
+                                    firebase.database().ref(`user_report_list`).orderByChild(`${user.uid}`).equalTo(true).once('value', (snapshot) => {
+                                        if(snapshot.exists === true && snapshot.val() === true) {
+                    
+                                            // Alert user has been deleted, edit message...or create pop-up screen 
+                                            alert('You have been removed from raveltree due to violation of terms of services.')
+                                            firebase.auth().currentUser.delete(); 
+
+                                            //TODO: Update their profile to have null values 
+                    
+                                        } else {
+                                            
+                                        }
+                                    })
                                 }
                             });
                             
