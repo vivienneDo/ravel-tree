e96eb36e1a941e2cf86a2af316a9c0bd5a0d6b57
Vivienne Do <viviennedo94@gmail.com>
2018-04-13 09:58:52 -0400

    - 04/12/18 - VD Do - Uncommented out ban user functions atm, fixed unhandled promise rejection

--- raveltree/src/utils/FBLoginComponent.js
+++ raveltree/src/utils/FBLoginComponent.js
@@ -5,6 +5,7 @@ Then, use the component as any other component
 ex: <FBLoginComponent/>
 
 - 04/07/18 - VD Do - Modified to set initial ravel_created field to false
+- 04/12/18 - VD Do - Uncommented out ban user functions atm, fixed unhandled promise rejection 
 */
 
 import React, { Component } from 'react';
@@ -94,22 +95,23 @@ class FBLoginComponent extends Component {
                                                 firebase.database().ref(`users/${user.uid}/ravel_created`).set(false)
 
                                             })
-                                } else {
-                                    // Check if they have been banned
-                                    firebase.database().ref(`user_report_list`).orderByChild(`${user.uid}`).equalTo(true).once('value', (snapshot) => {
-                                        if(snapshot.exists === true && snapshot.val() === true) {
+                                 } 
+                                // else {
+                                //     // Check if they have been banned
+                                //     firebase.database().ref(`user_report_list`).orderByChild(`${user.uid}`).equalTo(true).once('value', (snapshot) => {
+                                //         if(snapshot.exists === true && snapshot.val() === true) {
 
-                                            // Alert user has been deleted, edit message...or create pop-up screen
-                                            alert('You have been removed from raveltree due to violation of terms of services.')
-                                            firebase.auth().currentUser.delete();
+                                //             // Alert user has been deleted, edit message...or create pop-up screen
+                                //             alert('You have been removed from raveltree due to violation of terms of services.')
+                                //             firebase.auth().currentUser.delete();
 
-                                            //TODO: Update their profile to have null values
+                                //             //TODO: Update their profile to have null values
 
-                                        } else {
+                                //         } else {
 
-                                        }
-                                    })
-                                }
+                                //         }
+                                //     })
+                                // }
                             });
 
                         }
@@ -130,7 +132,7 @@ class FBLoginComponent extends Component {
 
                     new GraphRequestManager().addRequest(infoRequest).start();
                             })
-                            .catch(this.onAuthFailed.bind(this));
+                            .catch(this.onAuthFailed);
 
                         console.log('Attempting log in with facebook');
 
@@ -138,6 +140,9 @@ class FBLoginComponent extends Component {
                     console.log(error);
                     }
                 )
+                .catch((error) => {
+                (this.onAuthFailed)
+                })
                 }
             }
             }
