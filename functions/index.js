afd078593be4b36392f72f23002571a65042e53d
Chris Buruchian <EpiphX@gmail.com>
2018-04-16 20:27:53 -0400

    Updated upvote function such that it will only be called when the upvote field has changed.

--- functions/index.js
+++ functions/index.js
@@ -3,32 +3,46 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
 admin.initializeApp();
-exports.upVoted = functions.database.ref('/passages/{ravelId}/{passageId}')
+exports.upVoted = functions.database.ref('/passages/{ravelId}/{passageId}/passage_upvote')
     .onUpdate((change, context) => {
         const before = change.before.val();
         const after = change.after.val();
+        const user_uid = context.auth.uid;
 
-        var previousUpVoteValue = before['passage_upvote'];
-        var newUpVoteValue = after['passage_upvote'];
-
-        if (newUpVoteValue !== previousUpVoteValue) {
+        if (before !== after) {
             console.log('Handling new up vote value');
-
-            var type = 'upvoted';
-            var authorOfPassage = after['user_created'];
-            var user_uid = context.auth.uid;
-            var ravel_uid = after['ravel_uid'];
-            var passage_title = after['passage_title'];
-            var passage_uid = after['passage_uid'];
-            var upvotes = after['passage_combined_vote'];
 
             // Future: Post notification to OneSignal. 
             // Kick off promise to create new section in notification table.
+
+            return admin.database().ref('/passages/' + context.params.ravelId + '/' + context.params.passageId).once('value')
+                .then((snapshot) => {
+                    var passage = snapshot.val();
+
+                    var type = 'upvoted';
+                    var authorOfPassage = passage['user_created'];
+                    var ravel_uid = passage['ravel_uid'];
+                    var passage_title = passage['passage_title'];
+                    var passage_uid = passage['passage_uid'];
+                    var upvotes = passage['passage_combined_vote'];    
+                    
+                    return admin.database().ref('/users/' + user_uid + '/userProfile/first_name').once('value')
+                    .then((snapshot) => {
+                        return admin.database().ref('/notifications/' + authorOfPassage + '/').push({type, user_uid, first_name: snapshot.val(), ravel_uid, passage_uid, passage_title, upvotes});
+                    });
+                });
+
+            // var type = 'upvoted';
+            // var authorOfPassage = after['user_created'];
+            // var user_uid = context.auth.uid;
+            // var ravel_uid = after['ravel_uid'];
+            // var passage_title = after['passage_title'];
+            // var passage_uid = after['passage_uid'];
+            // var upvotes = after['passage_combined_vote'];
+
+
         
-            return admin.database().ref('/users/' + user_uid + '/userProfile/first_name').once('value')
-            .then((snapshot) => {
-                return admin.database().ref('/notifications/' + authorOfPassage + '/').push({type, user_uid, first_name: snapshot.val(), ravel_uid, passage_uid, passage_title, upvotes});
-            });
+
         } else {
             console.log('Old up vote value');
             return null;            
